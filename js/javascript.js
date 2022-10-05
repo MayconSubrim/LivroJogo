17b58d34f86d8b87749739cf1d3beaccee2b581d
MayconSubrim <maycon.subrim2002@gmail.com>
2022-10-05 03:27:54 -0300

    Foi adcionado mais paginas e concertado o modo nortuno

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,25 @@
-var pagina1 = `<h3>apenas para testes</h3>
-        <img src="https://www.comboinfinito.com.br/principal/wp-content/uploads/2021/11/jojo-stone-ocean-1.jpg" alt="">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias nobis sint facilis eveniet, vero quas modi velit excepturi praesentium nulla nihil enim qui doloribus eaque, officia accusamus vel unde rerum recusandae cumque. Quod dolore atque nulla, autem doloribus, deleniti, laboriosam veniam ratione repellendus magnam ut ea minus ad error enim aut ipsam iste sed eius labore. Expedita, sequi fugit. Distinctio quia, vitae possimus reiciendis, corrupti culpa molestiae aperiam at sunt asperiores, ab odit exercitationem voluptatum doloribus cumque veniam rem totam! Iusto sapiente facilis, odio debitis voluptatibus repellendus ipsum quasi consectetur blanditiis reprehenderit. Culpa optio aperiam, ullam cumque distinctio atque harum ab mollitia, animi ea, deserunt ratione temporibus dolore reiciendis. Unde architecto temporibus numquam voluptate aspernatur. Quisquam, eius cum? Velit quae id illum provident esse nesciunt adipisci voluptates doloribus autem, facilis tenetur consequuntur ullam harum nemo fugit. Fugit, vel sint. Dolor voluptas quas molestiae reprehenderit, facere placeat labore fuga accusantium quasi ad tempora, cupiditate veritatis optio nam fugit iure? Explicabo suscipit doloremque sapiente ab. Incidunt repellendus voluptatem optio sapiente dolorem. Eveniet illo fugiat labore aperiam inventore ut explicabo repellendus. Numquam ad necessitatibus sequi, quasi deleniti commodi optio eaque ducimus minima velit explicabo dolorem quis. Rerum nam expedita provident rem. Odio, minus.</p>
-        <button onclick="carregarConteudo(pagina2)">ir para esquerda</button>
-        <button onclick="carregarConteudo(pagina3)">ir para direita</button>`;
+var pagina1 = ` <section>
+<p>Certa vez, não muito distante da floresta e nem muito perto dela, havia uma bruxa, qual era famosa por realizar desejos impossiveis de serem feitos por humanos, porem a bruxa só realizaria esse desejo se tivesse algo em troca dele.
+<br>
+<br>
+Não tão longe e nem tão perto da bruxa, havia um menino, solitário e que ansiava mais do que nunca pela felicidade.
+O mesmo menino colecionava pesadelos, todos os dias o garoto era alimentado por lembranças ruins do passado que o assombrava repetinamente e todos os dias ele tinha medo de dormir.
+<br>
+<br>
+Cansado de ser assombrado pelos seus pesadelos, e sabendo que havia uma bruxa o mesmo resolveu que talvez ela o poderia ajudar
+<br>
+<br>
+Então o menino adentrou a floresta e foi em direção a bruxa, foi aí que percebeu dois caminhos:
+<br>
+<br>
+Um levava ele até a Bruxa e o outro a pequena casa do lenhador</p>
+</section>
+
+<img class="imagemzada" src="imagens/caminho floresta.png" alt="">
+<div class="opcaos">
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina2)">opção 1</button><p>Ir até a bruxa</p></div>
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina14)">opção 2</button><p>Ir até o lenhador</p></div>
+</div>`;
 
 var pagina2 = `<h3>apenas para testes</h3>
 <img src="https://criticalhits.com.br/wp-content/uploads/2021/06/jojo4.jpg" alt="">
@@ -16,6 +33,164 @@ var pagina3 = `<h3>apenas para testes</h3>
 <button>ir para esquerda</button>
 <button>ir para direita</button>`;
 
+var pagina9 = `<section>  
+<p>
+A bruxa, impaciente com sua indecisão, perguntou em um tom doce” Meu caro, o que achas de trocarmos de sonhos ? Poderias livrar-te de seus horríveis pesadelos e ficar com meus doce sonhos.” A bruxa sorri com ternura.
+Opçao; aceitar
+<br>
+O garoto, inclina o rosto com curiosidade.
+</p>
+</section>
+<img class="imagemzada" src="imagens/" alt="">
+<br>
+<br>
+
+<div class="opcaos">
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina10)">opção 1</button><p>Aceitar</p></div>
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina12)">opção 2</button><p>Recusar</p></div>
+</div>  `
+
+var pagina14 = ` <section>
+<p>O garoto então pensou e chegou a conclusão de que talvez pudesse não ter nada o que oferecer a Bruxa, seguiu em caminho ao lenhador e o avistou e foi aí que o garoto sentiu arrependimento, o lenhador era grande, tinha um semblante assustador e usava um machado que era tão grande quanto ele.
+O lenhador começou a ir em direção ao garoto.
+
+</p>
+</section>
+
+<img class="imagemzada" src="imagens/garotofloresta2.png" alt="">
+<div class="opcaos">
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina15)">opção 1</button><p>Ficar parado</p></div>
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina17)">opção 2</button><p>Correr</p></div>
+</div>`
+
+var pagina15 = `<section>
+<p>Ao se aproximar o lenhador pergunta: 
+<br>
+<br>
+- "o que faz aqui?" 
+então o garota amedrontado respondeu:
+Pensei em ir até a Bruxa, porem não creio que tenha algo a oferecer a ela e resolvi desviar o meu caminhado
+<br>
+<br>
+-"Todos sempre temos algo a oferecer, uns mais e outros menos, mas sempre algo. O que voce tanto deseja que a bruxa te desse?"                
+</p>
+</section>
+
+<img class="imagemzada" src="imagens/lenhador.png" alt="">
+<div class="opcaos">
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina16)">opção 1</button><p>Mentir e voltar pra casa</p></div>
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina18)">opção 2</button><p>Falar a verdade e ver o que o lenhador tem a dizer</p></div>
+</div>`
+
+var pagina16 = `<section>
+<p>O garoto com vergonha de falar o que queria inventa uma desculpa:
+-”É que sabe, na escola eu sofro muito bulying, e eu queria ser mais forte para afastar os valentões que ficam enchendo o meu saco”
+<br>
+<br>
+O lenhador supreso com o motivo explana:
+<br>
+<br>
+-”É só isso? poxa, você ate pode conseguir mais força, porem vai continuar sendo uma pessoa fraca, por ter que ir atraz de uma bruxa para resolver seus problemas. Quer uma dica? volte para casa e reflita em outros metodos que voce pode fazer para despistar esses valentões.”
+<br>
+<br>
+O menino fez que sim com a cabeça e saiu dali com a mesma velocidade de quem  é chamado pela mãe para o almoço.
+<br>
+<br>
+Porem ele segue um caminho diferente, com que faz ele se encontrar com a bruxa. 
+</section>
+<img class="imagemzada" src="imagens/garoto floresta.png" alt="">
+<div class="opcaos">
+<div class="optxt_center"><button class="options_center" onclick="carregarConteudo(pagina2)">Proxima pagina</button></div></p>
+</div>`
+
+var pagina17 = ` <section>
+<p>
+        Então o garoto correu mais rápido do que nunca, porém o lenhador era  tão grande que era impossível o menino fugir dele, o lenhador parou em sua frente e disse:
+        <br>
+        <br>
+        - "o que faz aqui?"
+</p>  
+</section>
+<img class="imagemzada" src="imagens/lenhador.png" alt="">
+<div class="opcaos">
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina18)">opção 1</button><p>Falar a verdade e ver o que o lenhador tem a oferecer</p></div>
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina16 )">opção 2</button><p>Mentir</p></div>
+</div>   `
+
+var pagina18 = `<section>
+<p>
+        O garoto então pensou, que talvez assim como a Bruxa o lenhador o pudesse ajudar e respondeu:
+        <br>
+        <br>
+        "tenho pesadelos de lembranças ruins todas as noites, desejo mais que tudo me livrar delas e me tornar uma garoto feliz"
+        <br>
+        <br>
+        o lenhador então começou a gargalhar, se ajoelhou na frente do garoto e disse:
+        <br>
+        <br>
+        "veja bem garoto, o que você seria sem as lembranças ruins? Ser feliz é sentir dores e as tristezas com a consciência de que elas fazem parte de nós,  é através delas que vamos nos aproximando cada vez mais do nosso propósito e é com elas que sabemos identificar a felicidade, se não houvesse dias chuvosos jamais saberíamos apreciar dias ensolarado" - o lenhador sorrindo  continuo:
+        "Vá, e se torne um grande homem, cujo transformar o sofrimento na sua maior arma"
+        <br>
+        <br>
+        O garoto então sorriu, as palavras que o lenhador havia lhe dito trouxerem a ele esperança, então após agradecer o lenhador o menino novamente adentrou a floresta, feliz, pensando em tudo que o lenhador havia te dito, até que avistou a bruxa, e se espantou: diferente do lenhador, ela tinha boa aparência e não causava medo algum, pelo contrário, era tão bela que fazia os olhos do menino brilhar.
+</p>
+</section>
+<img class="imagemzada" src="imagens/bruxa costa.jpg" alt="">
+<div class="opcaos">
+<div class="optxt_center"><button class="options_center" onclick="carregarConteudo(pagina19)">Proxima pagina</button></div>
+</div>`
+
+var pagina19 = `        <section>
+<p>
+        Ela então o encarou e disse:
+        "está perdido garoto?"
+        o garoto então respondeu:
+        <br>
+        <br>
+        "Estava em busca de algo, porém acho que já encontrei"
+        a Bruxa deu um sorriso largo, de orelha a orelha, se aproximou e disse:
+        <br>
+        <br>
+        "E o que tanto busca garoto?"
+        <br>
+        <br>
+        o garoto então ficou em silencio por um instante, mas resolveu responder:
+        <br>
+        <br>
+        "Vim atrás de algo que me fizesse esquecer meus pesadelos para que eu me torne um menino feliz"
+        a Bruxa o observando com atenção respondeu:
+        <br>
+        <br>
+        "posso lhe conceder dois caminhos: em um, posso apagar suas lembranças para que você possa ser livre delas, em outros, elas permanecem porem te darei poder e com ele você poderá ter tudo o que quiser, mas irá permanecer com elas."
+</p>
+</section>
+<img class="imagemzada" src="imagens/bruxagaroto.jpg" alt="">
+<div class="opcaos">
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina20 )">opção 1</button><p>Negar</p></div>
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina3)">opção 2</button><p>Aceitar ter lembramças pagadas</p></div>
+<div class="optxt"><button class="options" onclick="carregarConteudo(pagina6)">opção 3</button><p>aceitar poder</p></div>
+</div>`
+
+var pagina20 = `<section>  
+<p>
+        O garoto por um momento, ficou em silencio, ele sabia que poderia finalmente se livrar das lembranças indesejadas que o faziam ter tantos pesadelos, porem as palavras de esperança que o lenhador lhe concedeu ficaram gravadas em seu coração, o garoto virou as costas para a bruxa e respondeu:
+        <br>
+        <br>
+        "Obrigada senhora Bruxa, porem creio que o que eu tanto queria já me foi concedido"
+        O garoto foi embora, com as palavras do lenhador gravadas em seu coração  e chegou o dia em que se tornou um homem, cujo com sua grande resistência as lembranças ruins, se tornou um guerreiro imbatível, forte, destemido e que nunca desistia.
+</p>
+</section>
+<img class="imagemzada" src="imagens/garoto_velho.jpg" alt="">
+<br>
+<br>
+
+<div class="try">
+<p class="final">FIM.</p>
+<a href="/inicio.html"><div class="optxt_try"><button class="options_center">Jogar Novamente</button></a>
+</div>
+
+</div>`;
+
 var elemento = document.getElementById("conteudo");
 function carregarConteudo(conteudo) {
   elemento.innerHTML = conteudo;
@@ -26,16 +201,6 @@ var pagina = document.getElementById("corpo");
 
 function troca(){
         pagina.classList.toggle("altocontraste");
-        pagina.classList.remove("altocontraste1", "altocontraste2");
-
-}
-function troca1(){
-        pagina.classList.toggle("altocontraste1");
-        pagina.classList.remove("altocontraste", "altocontraste2");
-}
-function troca2(){
-        pagina.classList.toggle("altocontraste2");
-        pagina.classList.remove("altocontraste1", "altocontraste3");
 }
 
 carregarConteudo(pagina1)
